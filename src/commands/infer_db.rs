62808225d5f4fb11d965a3b67f8931445b9f153c
Emiliano Maldonado Garza <SrWilovy09@gmail.com>
2025-01-08 20:37:24 -0600

    chore(tools): ✨ Add cargo features

--- src/commands/infer_db.rs
+++ src/commands/infer_db.rs
@@ -6,11 +6,13 @@ pub async fn infer_database(
 ) -> Result<String, Box<dyn Error>> {
     if let Some(scheme) = database_url.split("://").next() {
         match scheme {
+            #[cfg(feature = "libsql")]
             "libsql" => {
                 println!("LibSQL");
                 grow_libsql::run_seeder(database_url, file_name).await;
                 Ok("LibSQL".to_string())
             }
+            #[cfg(feature = "sqlx")]
             "postgres" | "mysql" | "sqlite" => {
                 println!("SQLx database detected: {}", scheme);
                 grow_sqlx::run_seeder(&database_url, file_name).await?;
