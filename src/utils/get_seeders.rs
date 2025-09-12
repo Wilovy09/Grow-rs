6a95f2b70102bc29dd3dea7e80697a08b6a7ddf3
wilovy <computadora@Computadoras-MacBook-Pro.local>
2025-09-11 22:54:35 -0600

    feat: create list_seeders

--- src/utils/get_seeders.rs
+++ src/utils/get_seeders.rs
@@ -33,3 +33,34 @@ pub fn map_io_error(path: impl fmt::Debug) -> impl Fn(io::Error) -> String {
         }
     }
 }
+
+pub async fn list_seeders() -> Result<Vec<String>, String> {
+    let seeders_path = get_seeders().await?;
+
+    let mut entries = fs::read_dir(&seeders_path)
+        .await
+        .map_err(map_io_error(&seeders_path))?;
+
+    let mut seeders = Vec::new();
+
+    while let Some(entry) = entries
+        .next_entry()
+        .await
+        .map_err(map_io_error(&seeders_path))?
+    {
+        let path = entry.path();
+        if path.is_file() {
+            if let Some(extension) = path.extension() {
+                if extension == "ron" {
+                    if let Some(file_stem) = path.file_stem() {
+                        if let Some(name) = file_stem.to_str() {
+                            seeders.push(name.to_string());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(seeders)
+}
